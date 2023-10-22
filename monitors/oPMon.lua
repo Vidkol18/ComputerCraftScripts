0ff87c2cebbda047fd561249796cc5cc20b90492
Jacob Riley <Vidkol18@gmail.com>
2023-10-22 19:19:55 +0100

    Didn't make the monitor check for new messages. derp

--- monitors/oPMon.lua
+++ monitors/oPMon.lua
@@ -1,17 +1,22 @@
 print('ID: ' .. os.computerID())
 print('Running...')
-term.redirect(peripheral.wrap('top'))
+monitor = peripheral.wrap('top')
+term.redirect(monitor)
+monitor.setTextScale(0.5)
 
 local modemPort = 'bottom'
 
 rednet.close(modemPort) -- Always close just in case it is already open.
 rednet.open(modemPort)
 
-senderID, message, distance = rednet.receive()
+
 
 while true do
     term.clear()
     term.setCursorPos(1, 1)
+
+    senderID, message, distance = rednet.receive()
     print(message)
+
     os.sleep(1)
 end
